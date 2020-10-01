4f2e4ac622aa8a5610a8fe29aa4e6d611e94a599
Mathew Thomson <tmathew@woolworths.com.au>
2020-10-01 11:15:36 +1000

    adding ex 3

---
+++ ex3.py
@@ -0,0 +1,11 @@
+name = 'Thomson'
+Age = '32'
+weight = '69.5'
+eyes = 'black'
+teeth = 'white'
+hair = 'black'
+
+print (f"Let's talk about {name}")
+print (f"He's {Age} yrs old")
+print (f"and weighs {weight} kgs")
+print (f"He's got {eyes} eyes, {teeth} teeth and {hair} hair")
